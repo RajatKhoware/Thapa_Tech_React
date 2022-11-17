84b8dc87987ed64a4f8e63a7d3ad32849b384acb
Rajat Khoware <rajatkhoware2002@gmail.com>
2022-11-17 10:59:53 +0530

    1st commit 1_pro

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Resturant from './component/basic/Resturant'
+
+const App = () => {
+  return (
+    <div>
+      <Resturant/>
+    </div>
+  )
+}
+
+export default App
+
